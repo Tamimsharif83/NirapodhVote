f30e01ba42700bba57d9f46543de93f16092d1ff
Tamim Sharif <202314083@student.mist.ac.bd>
2025-12-21 07:36:12 +0600

    reg & sinnin pages frontend icons  updated

--- assets/js/common.js
+++ assets/js/common.js
@@ -24,10 +24,20 @@ function togglePassword(inputId) {
     const input = document.getElementById(inputId);
     if (!input) return;
     
+    const icon = input.parentElement.querySelector('i');
+
     if (input.type === 'password') {
         input.type = 'text';
+        if (icon) {
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        }
     } else {
         input.type = 'password';
+        if (icon) {
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
     }
 }
 
